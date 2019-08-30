0c40b293aa0b4a7ca646cada76778bae0805e0bb
Nick de Jong <nick.dejong@morningstar.com>
2019-08-30 15:22:02 -0500

    added config for demo

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,34 @@
-module.exports = {}
+module.exports = {
+  runtimeCompiler: true, // https://cli.vuejs.org/config/#runtimecompiler
+  outputDir: 'html-plugin-errors-dist',
+  css: {
+    loaderOptions: {
+      scss: {
+        data: `@import "~@/assets/stylesheet-hot-reload-styles.scss";`
+      }
+    }
+  },
+  chainWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.entry('styles-a')
+        .add('./src/assets/stylesheet-a.scss')
+        .end()
+      config.entry('styles-b')
+        .add('./src/assets/stylesheet-b.scss')
+        .end()
+      config.entry('styles-c')
+        .add('./src/assets/stylesheet-c.scss')
+        .end()
+      config.entry('styles-d')
+        .add('./src/assets/stylesheet-d.scss')
+        .end()
+      config.plugin('html')
+        .tap(([config, ...args]) => {
+          config.excludeChunks = ['styles-a', 'stylesheet-b.scss', 'styles-c.css']
+          return [config, ...args]
+        })
+        .end()
+    }
+      
+  }
+}
